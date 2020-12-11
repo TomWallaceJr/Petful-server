2615b978447e54c6eab852065f0dee9723436717
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2020-12-11 14:27:33 -0500

    Queue constructor and methods written

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -5,20 +5,22 @@ class _Node {
     this.next = null;
   }
 }
-
+// Q constructor inital data set to null
 class Queue {
   constructor() {
     // Set initial data.
+    this.first = null;
+    this.last = null;
   }
 
   enqueue(data) {
     // Add some data to the queue.
+    // Insertion operation limited to one space.... the end of the Q
     const node = new _Node(data);
 
     if (this.first === null) {
       this.first = node
     }
-
     if (this.last) {
       this.last.next = node;
     }
@@ -28,14 +30,42 @@ class Queue {
 
   dequeue() {
     // Remove some data from the queue.
+    // We only remove from one place in the q (the beginning)
+    // if the queue is empty, there is nothing to return
+    if (this.first === null) {
+      return;
+    }
+    const node = this.first;
+    this.first = this.first.next;
+
+    //if this is the last item in the queue
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.value;
   }
 
   show() {
     // Return the next item in the queue.
+    if (!this.first === null) {
+      return null;
+    }
+    return this.first.next;
   }
+
 
   all() {
     // Return all items in the queue.
+    // puts each item from Q into an array one by one and returns array
+    let arr = [];
+    let node = queue.first;
+    arr.push(node.value);
+    node = node.next;
+    while (node) {
+      arr.push(node.value);
+      node = node.next;
+    }
+    return arr;
   }
 }
 
